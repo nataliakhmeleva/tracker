d0561a6977a9fbc8c7d5ff757dee9446c8a9be17
Natalia Khmeleva <nataliya-mar@mail.ru>
2021-01-15 17:39:04 +0500

    5. Tracker - хранилище [#442077]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -8,5 +8,8 @@ public class StartUI {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
         String currentDateTimeFormat = item.getCreated().format(formatter);
         System.out.println(currentDateTimeFormat);
+        Tracker tracker = new Tracker();
+        tracker.add(item);
+        System.out.println(tracker.findById(1));
     }
 }
--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -21,5 +23,27 @@ public class Tracker {
             }
         }
         return rsl;
+    }
+
+    public Item[] findAll() {
+        Item[] itemsWithoutNull = new Item[items.length];
+        for (int i = 0; i < items.length; i++) {
+            if (items[i] != null) {
+                itemsWithoutNull[size++] = items[i];
+            }
+        }
+        itemsWithoutNull = Arrays.copyOf(itemsWithoutNull, size);
+        return itemsWithoutNull;
+    }
+
+    public Item[] findByName(String key) {
+        Item[] itemsResult = new Item[items.length];
+        for (int i = 0; i < items.length; i++) {
+            if (items[i].getName().equals(key)) {
+                itemsResult[size++] = items[i];
+            }
+        }
+        itemsResult = Arrays.copyOf(itemsResult, size);
+        return itemsResult;
     }
 }
