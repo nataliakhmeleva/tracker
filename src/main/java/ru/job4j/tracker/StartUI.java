4a5a2ddbc7f874639a112ea254411d9a0988b31f
Natalia Khmeleva <nataliya-mar@mail.ru>
2021-01-17 15:03:38 +0500

    9. toString [#442076]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -4,12 +4,13 @@ import java.time.format.DateTimeFormatter;
 
 public class StartUI {
     public static void main(String[] args) {
-        Item item = new Item();
+        Item item = new Item(1, "XXX");
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
         String currentDateTimeFormat = item.getCreated().format(formatter);
         System.out.println(currentDateTimeFormat);
         Tracker tracker = new Tracker();
         tracker.add(item);
         System.out.println(tracker.findById(1));
+        item.toString();
     }
 }
