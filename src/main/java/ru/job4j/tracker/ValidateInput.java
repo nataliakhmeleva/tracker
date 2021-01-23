9ae2770592f0bacacb7f24db69e8c80d53681dc5
Natalia Khmeleva <nataliya-mar@mail.ru>
2021-01-23 16:06:35 +0500

    2. Рефакторинг - Шаблон Декоратор для валидатора. [#442014]

--- src/main/java/ru/job4j/tracker/ValidateInput.java
+++ src/main/java/ru/job4j/tracker/ValidateInput.java
@@ -23,7 +23,7 @@ public class ValidateInput implements Input{
                 value = input.askInt(question);
                 invalid = false;
             } catch (NumberFormatException nfe) {
-                System.out.println("Please enter validate data again.");
+                output.println("Please enter validate data again.");
             }
         } while (invalid);
         return value;
