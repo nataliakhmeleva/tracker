96d5b11a6dc11e690e19758aea722ad0bee2679f
Natalia Khmeleva <nataliya-mar@mail.ru>
2021-02-18 19:18:15 +0500

    2. Отсортировать департаменты [#441966]

---
+++ src/main/java/ru/job4j/collection/comparator/department/Departments.java
@@ -0,0 +1,35 @@
+package ru.job4j.collection.comparator.department;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Departments {
+
+    public static List<String> fillGaps(List<String> deps) {
+        Set<String> tmp = new LinkedHashSet<>();
+        for (String value : deps) {
+            String start = "";
+            for (String el : value.split("/")) {
+                if (start.equals("")) {
+                    start = el;
+                    tmp.add(start);
+                } else {
+                    tmp.add(start + "/" + el);
+                }
+            }
+        }
+        return new ArrayList<>(tmp);
+    }
+
+    public static void sortAsc(List<String> orgs) {
+        orgs.sort(Comparator.naturalOrder());
+    }
+
+    public static void sortDesc(List<String> orgs) {
+        orgs.sort(new DepDescComp());
+    }
+}
---
+++ src/test/java/ru/job4j/collection/comparator/department/DepDescCompTest.java
@@ -0,0 +1,36 @@
+package ru.job4j.collection.comparator.department;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.*;
+
+public class DepDescCompTest {
+
+    @Test
+    public void compare() {
+        int rsl = new DepDescComp().compare(
+            "K2/SK1/SSK2",
+            "K2/SK1/SSK1"
+        );
+        assertThat(rsl, greaterThan(0));
+    }
+
+    @Test
+    public void whenUpDepartmentGoBefore() {
+        int rsl = new DepDescComp().compare(
+            "K2",
+            "K2/SK1"
+        );
+        assertThat(rsl, lessThan(0));
+    }
+
+    @Test
+    public void whenUpDepartmentGoBefore1() {
+        int rsl = new DepDescComp().compare(
+            "K1/SK1",
+            "K2/SK1"
+        );
+        assertThat(rsl, greaterThan(0));
+    }
+
+}
