c7f802377b835e222da189cac30ea0a9eecf5870
Natalia Khmeleva <nataliya-mar@mail.ru>
2021-02-27 13:39:05 +0500

    Изменения в оформлении

--- src/main/java/ru/job4j/lambda/Attachment.java
+++ src/main/java/ru/job4j/lambda/Attachment.java
@@ -20,9 +20,6 @@ public class Attachment {
 
     @Override
     public String toString() {
-        return "{" +
-            "name='" + name + '\'' +
-            ", size=" + size +
-            '}';
+        return "{" + "name='" + name + '\'' + ", size=" + size + '}';
     }
 }
