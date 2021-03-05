6a9f65d7608ca15f1872f1b6f8f7658f36fbbe86
Natalia Khmeleva <nataliya-mar@mail.ru>
2021-03-05 12:05:42 +0500

    0. Stream API [#441808]

--- src/main/java/ru/job4j/stream/ArrayListStreams.java
+++ src/main/java/ru/job4j/stream/ArrayListStreams.java
@@ -8,7 +8,9 @@ public class ArrayListStreams {
 
     public static void main(String[] args) {
         ArrayList<Integer> array = new ArrayList<>(
-            List.of(3, 4, 6, 9, 10, 15, 19, 25, 26, 31, 38, 46, 51));
-        List<Integer> list = array.stream().filter(n -> n % 2 == 0).collect(Collectors.toList());
+            List.of(-3, 4, -6, 9, 10, 15, -19, 25, -26, 31, 38, -46, 51));
+        List<Integer> list = array.stream()
+            .filter(n -> n > 0)
+            .collect(Collectors.toList());
     }
 }
