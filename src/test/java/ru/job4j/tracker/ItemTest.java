0e94da327c07ec024c6bbe3d1a85efed1c4fad16
Natalia Khmeleva <nataliya-mar@mail.ru>
2022-06-08 19:49:04 +0300

    2. Трекер SQL [#1734 #310427]

--- src/test/java/ru/job4j/tracker/ItemTest.java
+++ src/test/java/ru/job4j/tracker/ItemTest.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -13,21 +15,27 @@ public class ItemTest {
 
     @Test
     public void whenSortItem() {
-        List<Item> items = new ArrayList<>(List.of(new Item(2, "Item name2"),
-            new Item(1, "Item name1")
+        List<Item> items = new ArrayList<>(List.of(new Item(2, "Item name2", LocalDateTime.now()),
+            new Item(1, "Item name1", LocalDateTime.now())
         ));
         Collections.sort(items, new SortItem());
         assertThat(items.toString(),
-            is("[Item{id=1, name='Item name1'}, " + "Item{id=2, name='Item name2'}]"));
+            is("[id: 1, name: Item name1, created: " + LocalDateTime.now()
+                .format(DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss"))
+                + ", id: 2, name: Item name2, created: " + LocalDateTime.now()
+                .format(DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss")) + "]"));
     }
 
     @Test
     public void whenReverseItem() {
-        List<Item> items = new ArrayList<>(List.of(new Item(2, "Item name2"),
-            new Item(1, "Item name1")
+        List<Item> items = new ArrayList<>(List.of(new Item(2, "Item name2", LocalDateTime.now()),
+            new Item(1, "Item name1", LocalDateTime.now())
         ));
         Collections.sort(items, new ReverseItem());
         assertThat(items.toString(),
-            is("[Item{id=2, name='Item name2'}, " + "Item{id=1, name='Item name1'}]"));
+            is("[id: 2, name: Item name2, created: " + LocalDateTime.now()
+                .format(DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss"))
+                + ", id: 1, name: Item name1, created: " + LocalDateTime.now()
+                .format(DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss")) + "]"));
     }
 }
