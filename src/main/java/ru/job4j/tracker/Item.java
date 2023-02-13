2f50eac03aea2bce60645cf33d36017be6ec5b22
Natalia Khmeleva <nataliya-mar@mail.ru>
2023-02-13 15:19:58 +0300

    1. Конфигурация Hibernate [#6872]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -2,16 +2,20 @@ package ru.job4j.tracker;
 
 import lombok.Data;
 
+import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Objects;
 
+@Entity
+@Table(name = "items")
 @Data
 public class Item implements Comparable<Item> {
 
     private static final DateTimeFormatter FORMATTER =
             DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
     private LocalDateTime created = LocalDateTime.now();
